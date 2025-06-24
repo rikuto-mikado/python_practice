1d35db0643e3977543f68385dd2e3796e3c3c196
Rikuto <mikadorikuto@gmail.com>
2025-06-24 23:03:29 +0900

    added some codes in python with using flask

--- introduction_site/app.py
+++ introduction_site/app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html", title="ホーム")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About Us")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", title="Contact Us")
 
 
 if __name__ == "__main__":
