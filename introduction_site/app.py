73eab1ff62804958170075ac0ebdc097d46c1c13
Rikuto <mikadorikuto@gmail.com>
2025-06-27 22:06:28 +0900

    added contact form

--- introduction_site/app.py
+++ introduction_site/app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html", title="ホーム")
+    return render_template("pages/home.html", title="ホーム")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="About Us")
+    return render_template("pages/about.html", title="About Us")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html", title="Contact Us")
+    return render_template("pages/contact.html", title="Contact Us")
 
 
 if __name__ == "__main__":
