32ac6df5fd6a840a12b04f2bdafe449fc67185e2
Rikuto <mikadorikuto@gmail.com>
2025-07-02 09:10:53 +0900

    updated my todo_site

--- todo_site/app.py
+++ todo_site/app.py
@@ -1,6 +1,28 @@
-from app import create_app
+import streamlit as st
+import function
 
-app = create_app()
+todos = function.get_todos()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+def add_todo():
+    todo = st.session_state["new_todo"].strip()
+    if todo:
+        todos.append(todo + "\n")
+        function.write_todos(todos)
+        st.session_state["new_todo"] = ""
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo.strip(), key=f"todo_{index}")
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        st.rerun()
+
+st.text_input(
+    label="", placeholder="Add new todo...", on_change=add_todo, key="new_todo"
+)
