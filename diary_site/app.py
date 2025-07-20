63ab9803b317f873f9042e6d9ec57331b1914772
Rikuto <mikadorikuto@gmail.com>
2025-07-20 18:29:07 +0900

    finish

--- diary_site/app.py
+++ diary_site/app.py
@@ -21,7 +21,7 @@ class Entry(db.Model):
 @app.route("/")
 def index():
     page = request.args.get("page", 1, type=int)
-    per_page = 10
+    per_page = 5
     entries = Entry.query.order_by(Entry.date.desc()).paginate(
         page=page, per_page=per_page
     )
